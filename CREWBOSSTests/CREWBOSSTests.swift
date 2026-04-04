7c7a68b0ebcfbc11fd2a7173211f984bf298ce0e
Adam Lewis <95544948+adamlew52@users.noreply.github.com>
2026-04-04 10:45:57 -0600

    Initial Commit

---
+++ CREWBOSSTests/CREWBOSSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CREWBOSSTests.swift
+//  CREWBOSSTests
+//
+//  Created by alew on 4/4/26.
+//
+
+import Testing
+@testable import CREWBOSS
+
+struct CREWBOSSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

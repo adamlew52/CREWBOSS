328037240b62d8ee45dc845aae8768b9dde9096c
Adam Lewis <95544948+adamlew52@users.noreply.github.com>
2026-04-04 20:44:48 -0600

    more toolbar updates

--- CREWBOSS/ContentView.swift
+++ CREWBOSS/ContentView.swift
@@ -81,7 +81,7 @@ struct ContentView: View {
         }
         
         .toolbarBackground(.visible, for: .tabBar)
-        .toolbarBackground(Color.white, for: .tabBar)
+        //.toolbarBackground(Color.white, for: .tabBar)
         //.tint(Color(red: 0.19, green: 0.44, blue: 0.31))
         .onReceive(NotificationCenter.default.publisher(for: .navigateToTarget)) { note in
             guard let tab = note.userInfo?["tab"] as? String else { return }
